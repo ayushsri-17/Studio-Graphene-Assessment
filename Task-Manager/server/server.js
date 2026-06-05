fcfb36dccc5fb8f9a30694e8cec2dd2b1d285b97
Ayush <ayushsri178@gmail.com>
2026-06-05 13:44:26 +0530

    Initialize Mongoose models and database connection

--- Task-Manager/server/server.js
+++ Task-Manager/server/server.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose")
 const cors = require("cors");
 const dotenv = require("dotenv");
+
 
 dotenv.config();
 
 const app = express();
+
+mongoose
+        .connect(process.env.MONGO_URI)
+        .then(() => console.log("Connected to MongoDB"))
+        .catch((err) => console.log(err));
+
 
 // Middleware
 app.use(cors());
